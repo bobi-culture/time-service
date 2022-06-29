0b706f6b366d567bb4ca1375f13f8030133d31d4
Namvar <namvar@gmail.com>
2022-06-30 00:01:46 +0200

    Add api/time endpoint

--- app.py
+++ app.py
@@ -1,13 +1,20 @@
 from flask import Flask
+from flask import jsonify
 from time import ctime
 
 
 app = Flask(__name__)
 
 
+@app.route("/api/time", methods = ['GET'])
+def ping():
+    data = {"time" : str(ctime())}    
+    return jsonify(data)
+
+
 @app.route("/", methods = ['GET', 'POST'])
-def get_time():    
-    return str(ctime())
+def home():    
+    return "Time service is up!"
 
 
 app.run(host='0.0.0.0', threaded = True, debug = True)
